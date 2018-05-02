2c9ae1c0e4e3824bc081a5af076106fe96326730
Baptiste Darthenay <baptiste@maykinmedia.nl>
2018-05-02 10:57:47 +0200

    Upgraded to 1.3.2

--- filer/__init__.py
+++ filer/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
 # version string following pep-0396 and pep-0386
-__version__ = '1.3.0-afs.9'  # pragma: nocover
+__version__ = '1.3.2.post10'  # pragma: nocover
 
 default_app_config = 'filer.apps.FilerConfig'
